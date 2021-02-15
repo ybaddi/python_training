af6a3632d654bc511dee6834bceb81e9bf0b9d7d
baddi <baddiyoussef85@gmail.com>
2021-02-15 18:47:15 +0100

    first folder to the project

---
+++ example_1/app.py
@@ -0,0 +1,8 @@
+# git clone  => cloner un projet from git url
+# git status => la situation de ton projet en local, 
+#               les folders/fichiers tracked/ untracked, edited (added or deleted)
+# git add <folder/file> => il va ajouter l'element pour etre dans l'index du tracking
+# git commit -m " "   => ajouter la modification as a commit dans l'index local
+# git push           => send all commits to the remote repository
+
+
