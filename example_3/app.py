77a30174587724f352a601d05d7b287fe03766b6
baddi <baddiyoussef85@gmail.com>
2021-02-15 19:43:47 +0100

    add example_3

---
+++ example_3/app.py
@@ -0,0 +1,10 @@
+class MonClasse:
+
+    def __init__(self, name, age):
+      self.name = name
+      self.age = age
+
+mon_object = MonClasse("baddi", 35)
+
+
+print("Bonjour, je suis {0} et j'ai {1}".format(mon_object.name, mon_object.age))
